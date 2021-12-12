1061d84e94677e4d59c88cdfde69405cbf1b0486
Frombull <marcorrditoro@gmail.com>
2021-12-12 20:20:27 -0300

    Refactored code

--- dungeon_scraper.py
+++ dungeon_scraper.py
@@ -1,10 +1,11 @@
-import logging as log
-from pprint import pprint
 import json
+import logging as log
+from dataclasses import dataclass
 
 import requests
 from bs4 import BeautifulSoup
 
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'}
 
 log.basicConfig(level=log.INFO,
                 format='[%(levelname)s] (%(asctime)s) - %(message)s',
@@ -13,21 +14,30 @@ log.basicConfig(level=log.INFO,
 log.info('Stating scraper')
 
 
-class DungeonScraper:
-    def __init__(self):
-        self.HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'}
+@dataclass()
+class Dungeons:
+    name: str = 'N/A'
+    type: str = 'N/A'
+    level: str = 'N/A'
+    ilevel: str = 'N/A'
+    expansion: str = 'N/A'
+    patch: str = 'N/A'
+    difficulty: str = 'N/A'
+    party_size: str = 'N/A'
+    url: str = 'N/A'
 
-    def getDungeon(self, dg_name: str):
-        dg_name_filtered = self._filterDgName(dg_name)
+    @classmethod
+    def get(cls, dg_name: str):
+        dg_name_filtered = cls._formatNameToUrl(dg_name)
         URL = f'https://ffxiv.consolegameswiki.com/wiki/{dg_name_filtered}'
 
         log.info(f'Getting dungeon: {dg_name}, from url: {URL}')
 
-        r = requests.get(url=URL, headers=self.HEADERS)
+        r = requests.get(url=URL, headers=HEADERS)
         soup = BeautifulSoup(r.content, 'html.parser')
 
         infobox = soup.find('div', class_='infobox-n duty')
-        dg_name = infobox.find('p', class_='heading').text.strip()
+        cls.name = infobox.find('p', class_='heading').text.strip()
 
         wrappers = infobox.find('div', class_='wrapper').find_all('dl')
 
@@ -38,67 +48,66 @@ class DungeonScraper:
                     wrapper_final.append(str(item).strip().lower())
 
         for i, item in enumerate(wrapper_final):
-            next_index = i+1
+            next_index = i + 1
             if next_index >= len(wrapper_final):
                 break
-
             next_item = wrapper_final[next_index]
+
             if '>difficulty<' in item:
-                dg_difficulty = self._filterTags(next_item)
+                cls.difficulty = cls._filterTags(next_item)
             elif '>level<' in item:
-                dg_level = self._filterTags(next_item)
+                cls.level = cls._filterTags(next_item)
             elif '>ilevel<' in item:
-                dg_ilevel = self._filterTags(next_item)
+                cls.ilevel = cls._filterTags(next_item)
             elif '>patch<' in item:
-                dg_patch = next_item.split('</a>')[0].split('">')[1]
-                dg_expansion = self._getExpansionFromPatch(dg_patch)
+                cls.patch = next_item.split('</a>')[0].split('">')[1]
+                cls.expansion = cls._getExpansionFromPatch(cls.patch)
             elif '>party size<' in item:
                 if 'Alliance' in next_item:
-                    dg_party_size = 'Alliance'
+                    cls.party_size = 'Alliance'
                 else:
-                    dg_party_size = self._filterTags(next_item)
-                    dg_party_size = (dg_party_size.split('Party')[0] + 'Party').title()
-
+                    cls.party_size = cls._filterTags(next_item)
+                    cls.party_size = (cls.party_size.split('Party')[0] + 'Party').title()
 
         main_page = soup.find('div', class_='mw-parser-output')
         for p in main_page.find_all('p'):
             if 'href="/wiki/Dungeon"' in str(p):
-                dg_type = 'Dungeon'
+                cls.type = 'Dungeon'
+                break
             elif 'href="/wiki/Trial"' in str(p):
-                dg_type = 'Trial'
+                cls.type = 'Trial'
+                break
             elif 'href="/wiki/Raid"' in str(p):
-                dg_type = 'Raid'
+                cls.type = 'Raid'
+                break
 
-        # TODO: Set everything as None by default
-        return {
-            "name": dg_name,
-            "type": dg_type,
-            "level": dg_level,
-            "ilevel": dg_ilevel,
-            "expansion": dg_expansion,
-            "patch": dg_patch,
-            "dificulty": dg_difficulty,
-            "partySize": dg_party_size,
-            "url": URL
-        }
+        return cls(
+            name=cls.name,
+            type=cls.type,
+            level=cls.level,
+            ilevel=cls.ilevel,
+            expansion=cls.expansion,
+            patch=cls.patch,
+            difficulty=cls.difficulty,
+            party_size=cls.party_size,
+            url=URL
+        )
 
     @staticmethod
-    def _filterDgName(dg_name: str) -> str:
+    def _formatNameToUrl(dg_name: str) -> str:
         log.info(f'Filtering dungeon name: {dg_name}')
         return dg_name.strip().replace(' ', '_').replace("'", '%27').title()
 
     @staticmethod
     def _filterTags(item: str) -> str:
-        # log.info('Filtering item tags')
         try:
             return item[1:].split('<')[0].split('>')[1].replace('\xa0', '').title()
         except IndexError:
             return item.replace('\xa0', '').title()
-        except:
-            return item
 
     @staticmethod
     def _getExpansionFromPatch(patch: str) -> str:
+        patch_first_num = str(patch.split('.')[0])
         patches = {
             "2": "A Realm Reborn",
             "3": "Heavensward",
@@ -106,9 +115,11 @@ class DungeonScraper:
             "5": "Shadowbringers",
             "6": "Endwalker"
         }
-        return patches[patch.split('.')[0]]
+        return patches.get(patch_first_num, 'N/A')
+
+    def __str__(self):
+        return json.dumps(vars(self), indent=2)
 
 
 if __name__ == '__main__':
-    dungeon_info = DungeonScraper().getDungeon("Amdapor Keep")
-    print(json.dumps(dungeon_info, indent=4))
+    print(Dungeons.get('Amdapor keep'))
