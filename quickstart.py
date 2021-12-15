d4fe3451a586bf7abab47a9c1417307f1b3a3afa
Frombull <marcorrditoro@gmail.com>
2021-12-15 01:49:24 -0300

    2AM but it works

--- quickstart.py
+++ quickstart.py
@@ -1,10 +1,9 @@
-from __future__ import print_function
-
 import logging as log
 
 from googleapiclient.discovery import build
 
 from constants import *
+
 
 log.basicConfig(level=log.INFO,
                 format='[%(levelname)s] (%(asctime)s) - %(message)s',
@@ -14,6 +13,8 @@ service = build('sheets', 'v4', credentials=CREDS)
 
 
 def readFromDocs(*, id: str, range: str):
+    log.info(f'Reading from table id: {id}, with range: {range}')
+
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=id,
                                 range=range).execute()
@@ -23,23 +24,11 @@ def readFromDocs(*, id: str, range: str):
 
 
 def writeInDocs(*, id: str, range: str, write_body: list):
+    log.info(f'Writing in table id: {id}, with range: {range}')
+
     request = service.spreadsheets().values().update(spreadsheetId=id, range=range,
                                                      valueInputOption="RAW",
                                                      body={"values": write_body})
     response = request.execute()
 
     return response if response else log.error('No data written')
-
-
-if __name__ == '__main__':
-    # print(readFromDocs(id='1iAC6O-_9YSG47CYiTWTvMNL4Li-EaEjuNUZOHgkS4lI',
-    #                    range='A1:A1000'))
-
-    test_body = [['test1'],
-                 ['test2'],
-                 ['test3'],
-                 ['test4']]
-    writeInDocs(id='1iAC6O-_9YSG47CYiTWTvMNL4Li-EaEjuNUZOHgkS4lI',
-                range='A1:a1000',
-                write_body=test_body)
-    pass
