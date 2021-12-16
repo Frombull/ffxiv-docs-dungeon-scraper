c0f9db0848f8cfea1e549331ab8716d6d65dd6a8
Frombull <marcorrditoro@gmail.com>
2021-12-16 00:07:57 -0300

    Added a map for each column data type

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
+import json
 import logging as log
 
+from constants import *
 from dungeon_scraper import Dungeons
-from quickstart import readFromDocs, writeInDocs
-
+from google_docs_functions import *
 
 log.basicConfig(level=log.INFO,
                 format='[%(levelname)s] (%(asctime)s) - %(message)s',
                 datefmt='%H:%M:%S')
 
+DOC_ID = '1iAC6O-_9YSG47CYiTWTvMNL4Li-EaEjuNUZOHgkS4lI'
+
 
 def main():
-    dungeons_read = readFromDocs(id='1iAC6O-_9YSG47CYiTWTvMNL4Li-EaEjuNUZOHgkS4lI',
-                                 range='A1:A100')
+
+    fist_row = get_first_row(id=DOC_ID)[0]
+    first_row_map = make_first_row_map(fist_row)
+    #print(filtered_first_row)
+
+
+    dungeons_read = readFromDocs(id=DOC_ID,
+                                 range=f'{first_row_map["name"]}1:{first_row_map["name"]}100')
 
     dungeons_json = {}
     for i, dungeon_row in enumerate(dungeons_read, 1):
         dungeon_name = str(dungeon_row[0])
         dungeon_info = Dungeons.get(dungeon_name)
-        print(f'[{i}]...')
 
-        dungeons_json[f'dungeon_{i}'] = dungeon_info
+        if dungeon_info:
+            print(f'[{i}]...')
+            dungeons_json[f'dungeon_{i}'] = dungeon_info
+        else:
+            print(f'[{i}]xxx')
+            dungeons_json[f'dungeon_{i}'] = NONE_DICT
 
-    print('-' * 80)
+    print(json.dumps(dungeons_json, indent=2))
 
-    # print(json.dumps(dungeons_json, indent=2))
-    # print('-' * 80)
-
-    write_body = []
-    for dg_number in dungeons_json:
-        item = [dungeons_json[dg_number]['ilevel']]
-        write_body.append(item)
-
-    print(f'write_body = {write_body}')
-
-    writeInDocs(id='1iAC6O-_9YSG47CYiTWTvMNL4Li-EaEjuNUZOHgkS4lI',
-                range='B1:B1000',
-                write_body=write_body)
+    # write_body = []
+    # for dg_number in dungeons_json:
+    #     item = [dungeons_json[dg_number]['ilevel']]
+    #     write_body.append(item)
+    #
+    # print(f'write_body = {write_body}')
+    #
+    # writeInDocs(id=DOC_ID,
+    #             range='B1:B100',
+    #             write_body=write_body)
 
 
 if __name__ == '__main__':
