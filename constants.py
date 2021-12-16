9bbdd7840ab9bf0f53d655003057534094a89e7c
Frombull <marcorrditoro@gmail.com>
2021-12-16 00:06:47 -0300

    Added default dict

--- constants.py
+++ constants.py
@@ -3,3 +3,15 @@ from google.oauth2 import service_account
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = 'top_secret_keys.json'
 CREDS = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+NONE_DICT = {
+            "name": 'None',
+            "type": 'None',
+            "level": 'None',
+            "ilevel": 'None',
+            "expansion": 'None',
+            "patch": 'None',
+            "difficulty": 'None',
+            "party_size": 'None',
+            "url": 'None'
+        }
